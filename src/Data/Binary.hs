7b261df13fdaa0de5f2d4d521ab75f51fe6f2474
Don Stewart <dons@cse.unsw.edu.au>
2007-01-11 13:36:49 +0100

    clean ups

--- src/Data/Binary.hs
+++ src/Data/Binary.hs
@@ -46,6 +46,7 @@ import Data.Array.IArray
 import Data.Array.Unboxed (UArray)
 
 ------------------------------------------------------------------------
+-- 
 
 encode :: Binary a => a -> ByteString
 encode = runPutM . put
@@ -63,13 +64,13 @@ instance Binary () where
     put ()  = return ()
     get     = return ()
 
+------------------------------------------------------------------------
+-- Words
+
 instance Binary Word8 where
     put     = putWord8
     get     = getWord8
 
--- 
--- todo, investigate bit fields for sequences of Bools
---
 instance Binary Bool where
     put     = putWord8 . fromIntegral . fromEnum
     get     = liftM (toEnum . fromIntegral) getWord8
@@ -88,25 +89,50 @@ instance Binary Word64 where
 
 instance Binary Int8 where
     put i   = put (fromIntegral i :: Word8)
-    get     = fromIntegral `fmap` (get :: GetM Word8)
+    get     = liftM fromIntegral (get :: GetM Word8)
 
 instance Binary Int16 where
     put i   = put (fromIntegral i :: Word16)
-    get     = fromIntegral `fmap` (get :: GetM Word16)
+    get     = liftM fromIntegral (get :: GetM Word16)
 
 instance Binary Int32 where
     put i   = put (fromIntegral i :: Word32)
-    get     = fromIntegral `fmap` (get :: GetM Word32)
+    get     = liftM fromIntegral (get :: GetM Word32)
 
 instance Binary Int64 where
     put i   = put (fromIntegral i :: Word64)
-    get     = fromIntegral `fmap` (get :: GetM Word64)
+    get     = liftM fromIntegral (get :: GetM Word64)
 
 instance Binary Int where
     put i   = put (fromIntegral i :: Int32)
-    get     = fromIntegral `fmap` (get :: GetM Int32)
+    get     = liftM fromIntegral (get :: GetM Int32)
 
--- TODO Integer
+{-
+
+instance Binary Integer where
+    put (S# i)    = putWord8 0 >> put (I# i)
+    put (J# s ba) = do
+        putWord8 1
+        put (I# s)
+        let sz = sizeofByteArray# ba   -- in *bytes*
+        put (I# sz)                    -- in *bytes*
+        putByteArray a# sz#
+
+    get = do
+        b <- getWord8
+        case b of
+          0 -> do (I# i) <- get
+                  return (S# i)
+          _ -> do (I# s) <- get
+                  sz     <- get
+                  (BA a) <- getByteArray sz
+                  return (J# s a)
+
+data ByteArray = BA ByteArray#
+-}
+
+------------------------------------------------------------------------
+-- Char
 
 -- TODO profile, benchmark and test this instance
 instance Binary Char where
@@ -130,34 +156,77 @@ instance Binary Char where
         w = fromIntegral (shiftR c 18 .&. 0x7)
 
     get = do
-        let getByte = fmap (fromIntegral :: Word8 -> Int) get
+        let getByte = liftM (fromIntegral :: Word8 -> Int) get
             shiftL6 = flip shiftL 6 :: Int -> Int
         w <- getByte
         r <- case () of
                 _ | w < 0x80  -> return w
                   | w < 0xe0  -> do
-                                    x <- fmap (xor 0x80) getByte
+                                    x <- liftM (xor 0x80) getByte
                                     return (x .|. shiftL6 (xor 0xc0 w))
                   | w < 0xf0  -> do
-                                    x <- fmap (xor 0x80) getByte
-                                    y <- fmap (xor 0x80) getByte
+                                    x <- liftM (xor 0x80) getByte
+                                    y <- liftM (xor 0x80) getByte
                                     return (y .|. shiftL6 (x .|. shiftL6
                                             (xor 0xe0 w)))
                   | otherwise -> do
-                                x <- fmap (xor 0x80) getByte
-                                y <- fmap (xor 0x80) getByte
-                                z <- fmap (xor 0x80) getByte
+                                x <- liftM (xor 0x80) getByte
+                                y <- liftM (xor 0x80) getByte
+                                z <- liftM (xor 0x80) getByte
                                 return (z .|. shiftL6 (y .|. shiftL6
                                         (x .|. shiftL6 (xor 0x80 w))))
         return $! chr r
 
+------------------------------------------------------------------------
+-- Tuples
+
+instance (Binary a, Binary b) => Binary (a,b) where
+    put (a,b) = put a >> put b
+    get       = do a <- get
+                   b <- get
+                   return (a,b)
+
+--
+-- And then the recursive cases
+--
+
+instance (Binary a, Binary b, Binary c) => Binary (a,b,c) where
+    put (a,b,c)         = put (a, (b,c))
+    get                 = do (a,(b,c)) <- get ; return (a,b,c)
+
+instance (Binary a, Binary b, Binary c, Binary d) => Binary (a,b,c,d) where
+    put (a,b,c,d)       = put (a,(b,c,d))
+    get                 = do (a,(b,c,d)) <- get ; return (a,b,c,d)
+
+instance (Binary a, Binary b, Binary c, Binary d, Binary e) => Binary (a,b,c,d,e) where
+    put (a,b,c,d,e)     = put (a,(b,c,d,e))
+    get                 = do (a,(b,c,d,e)) <- get ; return (a,b,c,d,e)
+
+instance (Binary a, Binary b, Binary c, Binary d, Binary e, Binary f) => Binary (a,b,c,d,e,f) where
+    put (a,b,c,d,e,f)   = put (a,(b,c,d,e,f))
+    get                 = do (a,(b,c,d,e,f)) <- get ; return (a,b,c,d,e,f)
+
+instance (Binary a, Binary b, Binary c, Binary d
+         , Binary e, Binary f, Binary g) => Binary (a,b,c,d,e,f,g) where
+    put (a,b,c,d,e,f,g) = put (a,(b,c,d,e,f,g))
+    get                 = do (a,(b,c,d,e,f,g)) <- get ; return (a,b,c,d,e,f,g)
+
+------------------------------------------------------------------------
+-- Container types
+
 instance Binary a => Binary [a] where
-    put l  = do
-        put (length l)
-        mapM_ put l
-    get    = do
-        n <- get :: GetM Int
-        replicateM n get
+    put l  = put (length l) >> mapM_ put l
+    get    = do n <- get :: GetM Int
+                replicateM n get
+
+instance (Binary a) => Binary (Maybe a) where
+    put Nothing  = putWord8 0
+    put (Just x) = putWord8 1 >> put x
+    get = do
+        w <- getWord8
+        case w of
+            0 -> return Nothing
+            _ -> liftM Just get
 
 instance (Binary a, Binary b) => Binary (Either a b) where
     put (Left  a) = putWord8 0 >> put a
@@ -168,77 +237,45 @@ instance (Binary a, Binary b) => Binary (Either a b) where
             0 -> liftM Left  get
             _ -> liftM Right get
 
-instance (Binary a) => Binary (Maybe a) where
-    put Nothing  = putWord8 0
-    put (Just x) = putWord8 1 >> put x
-    get = do
-        w <- getWord8
-        case w of
-            0 -> return Nothing
-            _ -> fmap Just get
-
-instance (Binary a, Binary b) => Binary (a,b) where
-    put (a,b) = put a >> put b
-    get       = do a <- get
-                   b <- get
-                   return (a,b)
-
-instance (Binary a, Binary b, Binary c) => Binary (a,b,c) where
-    put (a,b,c) = put a >> put b >> put c
-    get = do
-       a <- get
-       b <- get
-       c <- get
-       return (a,b,c)
-
-instance (Binary a, Binary b, Binary c, Binary d) => Binary (a,b,c,d) where
-    put (a,b,c,d) = put a >> put b >> put c >> put d
-    get = do
-        a <- get
-        b <- get
-        c <- get
-        d <- get
-        return (a,b,c,d)
+------------------------------------------------------------------------
+-- ByteStrings (have specially efficient instances)
 
 instance Binary B.ByteString where
-    put bs = do
-        put (B.length bs)
-        putByteString bs
-    get = do
-        len <- get
-        getByteString len
+    put bs = do put (B.length bs)
+                putByteString bs
+    get    = get >>= getByteString
 
--- 
--- needs the newtyped' bytestring
--- 
 instance Binary ByteString where
-    put bs = do
-        put (L.length bs)
-        putLazyByteString bs
-    get = do
-        len <- get
-        getLazyByteString len
+    put bs = do put (L.length bs)
+                putLazyByteString bs
+    get    = get >>= getLazyByteString
+
+------------------------------------------------------------------------
+-- Maps and Sets
 
 instance (Ord a, Binary a) => Binary (Set.Set a) where
     put = put . Set.toAscList
-    get = fmap Set.fromDistinctAscList get
+    get = liftM Set.fromDistinctAscList get
 
 instance (Ord k, Binary k, Binary e) => Binary (Map.Map k e) where
     put = put . Map.toAscList
-    get = fmap Map.fromDistinctAscList get
+    get = liftM Map.fromDistinctAscList get
 
 instance Binary IntSet.IntSet where
     put = put . IntSet.toAscList
-    get = fmap IntSet.fromDistinctAscList get
+    get = liftM IntSet.fromDistinctAscList get
 
 instance (Binary e) => Binary (IntMap.IntMap e) where
     put = put . IntMap.toAscList
-    get = fmap IntMap.fromDistinctAscList get
+    get = liftM IntMap.fromDistinctAscList get
+
+------------------------------------------------------------------------
+-- Arrays
 
 instance (Binary i, Ix i, Binary e) => Binary (Array i e) where
     put a = do
         put (bounds a)
-        put $ elems a
+        put (elems a)
     get = do
         bs <- get
         es <- get
@@ -254,7 +291,7 @@ instance (Binary i, Ix i, Binary e) => Binary (Array i e) where
 instance (Binary i, Ix i, Binary e, IArray UArray e) => Binary (UArray i e) where
     put a = do
         put (bounds a)
-        put $ elems a
+        put (elems a)
     get = do
         bs <- get
         es <- get
