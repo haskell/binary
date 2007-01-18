0e175f9614d01ac21b7be1b3091bb8dab616606e
Duncan Coutts <duncan.coutts@worc.ox.ac.uk>
2007-01-18 15:14:20 +0100

    Add deserialisation to the benchmark The 'Get' side has not been optimised at all yet so it's doing about 3Mb/s compared to between 10 and 150MB/s for the 'Put' stuff

--- tests/Benchmark.hs
+++ tests/Benchmark.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Data.ByteString.Lazy as L
 import Data.Binary
 import Data.Binary.Put
+import Data.Binary.Get
 
 import Control.Exception
 import System.CPUTime
@@ -29,46 +30,50 @@ test wordSize chunkSize mb = do
     let bytes :: Int
         bytes = mb * 2^20
         iterations = bytes `div` wordSize
-        len = L.length $ runPut $ go wordSize chunkSize iterations
+        bs  = runPut (doPut wordSize chunkSize iterations)
+        sum = runGet (doGet wordSize chunkSize iterations) bs
     putStr $ show mb ++ "MB of Word" ++ show (8 * wordSize)
           ++ " in chunks of " ++ show chunkSize ++ ": "
-    seconds <- time $ evaluate len
+    putSeconds <- time $ evaluate (L.length bs)
+    getSeconds <- time $ evaluate sum
     --print len
-    let throughput = fromIntegral mb / seconds
-    putStrLn $ showFFloat (Just 2) throughput "MB/s"
+    let putThroughput = fromIntegral mb / putSeconds
+        getThroughput = fromIntegral mb / getSeconds
+    putStrLn $ showFFloat (Just 2) putThroughput "MB/s write, "
+            ++ showFFloat (Just 2) getThroughput "MB/s read"
 
-go :: Int -> Int -> Int -> Put
-go wordSize chunkSize =
+doPut :: Int -> Int -> Int -> Put
+doPut wordSize chunkSize =
   case (wordSize, chunkSize) of
-    (1, 1)  -> word8N1
-    (1, 2)  -> word8N2
-    (1, 4)  -> word8N4
-    (1, 8)  -> word8N8
-    (1, 16) -> word8N16
-    (2, 1)  -> word16N1
-    (2, 2)  -> word16N2
-    (2, 4)  -> word16N4
-    (2, 8)  -> word16N8
-    (2, 16) -> word16N16
-    (4, 1)  -> word32N1
-    (4, 2)  -> word32N2
-    (4, 4)  -> word32N4
-    (4, 8)  -> word32N8
-    (4, 16) -> word32N16
-    (8, 1)  -> word64N1
-    (8, 2)  -> word64N2
-    (8, 4)  -> word64N4
-    (8, 8)  -> word64N8
-    (8, 16) -> word64N16
+    (1, 1)  -> putWord8N1
+    (1, 2)  -> putWord8N2
+    (1, 4)  -> putWord8N4
+    (1, 8)  -> putWord8N8
+    (1, 16) -> putWord8N16
+    (2, 1)  -> putWord16N1
+    (2, 2)  -> putWord16N2
+    (2, 4)  -> putWord16N4
+    (2, 8)  -> putWord16N8
+    (2, 16) -> putWord16N16
+    (4, 1)  -> putWord32N1
+    (4, 2)  -> putWord32N2
+    (4, 4)  -> putWord32N4
+    (4, 8)  -> putWord32N8
+    (4, 16) -> putWord32N16
+    (8, 1)  -> putWord64N1
+    (8, 2)  -> putWord64N2
+    (8, 4)  -> putWord64N4
+    (8, 8)  -> putWord64N8
+    (8, 16) -> putWord64N16
 
-word8N1 = loop 0
+putWord8N1 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
           putWord8 (s+0)
           loop (s+1) (n-1)
 
-word8N2 = loop 0
+putWord8N2 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -76,7 +81,7 @@ word8N2 = loop 0
           putWord8 (s+1)
           loop (s+2) (n-2)
 
-word8N4 = loop 0
+putWord8N4 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -86,7 +91,7 @@ word8N4 = loop 0
           putWord8 (s+3)
           loop (s+4) (n-4)
 
-word8N8 = loop 0
+putWord8N8 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -100,7 +105,7 @@ word8N8 = loop 0
           putWord8 (s+7)
           loop (s+8) (n-8)
 
-word8N16 = loop 0
+putWord8N16 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -123,14 +128,14 @@ word8N16 = loop 0
           loop (s+16) (n-16)
 
 
-word16N1 = loop 0
+putWord16N1 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
           putWord16be (s+0)
           loop (s+1) (n-1)
 
-word16N2 = loop 0
+putWord16N2 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -138,7 +143,7 @@ word16N2 = loop 0
           putWord16be (s+1)
           loop (s+2) (n-2)
 
-word16N4 = loop 0
+putWord16N4 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -148,7 +153,7 @@ word16N4 = loop 0
           putWord16be (s+3)
           loop (s+4) (n-4)
 
-word16N8 = loop 0
+putWord16N8 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -162,7 +167,7 @@ word16N8 = loop 0
           putWord16be (s+7)
           loop (s+8) (n-8)
 
-word16N16 = loop 0
+putWord16N16 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -185,14 +190,14 @@ word16N16 = loop 0
           loop (s+16) (n-16)
 
 
-word32N1 = loop 0
+putWord32N1 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
           putWord32be (s+0)
           loop (s+1) (n-1)
 
-word32N2 = loop 0
+putWord32N2 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -200,7 +205,7 @@ word32N2 = loop 0
           putWord32be (s+1)
           loop (s+2) (n-2)
 
-word32N4 = loop 0
+putWord32N4 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -210,7 +215,7 @@ word32N4 = loop 0
           putWord32be (s+3)
           loop (s+4) (n-4)
 
-word32N8 = loop 0
+putWord32N8 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -224,7 +229,7 @@ word32N8 = loop 0
           putWord32be (s+7)
           loop (s+8) (n-8)
 
-word32N16 = loop 0
+putWord32N16 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -246,14 +251,14 @@ word32N16 = loop 0
           putWord32be (s+15)
           loop (s+16) (n-16)
 
-word64N1 = loop 0
+putWord64N1 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
           putWord64be (s+0)
           loop (s+1) (n-1)
 
-word64N2 = loop 0
+putWord64N2 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -261,7 +266,7 @@ word64N2 = loop 0
           putWord64be (s+1)
           loop (s+2) (n-2)
 
-word64N4 = loop 0
+putWord64N4 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -271,7 +276,7 @@ word64N4 = loop 0
           putWord64be (s+3)
           loop (s+4) (n-4)
 
-word64N8 = loop 0
+putWord64N8 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -285,7 +290,7 @@ word64N8 = loop 0
           putWord64be (s+7)
           loop (s+8) (n-8)
 
-word64N16 = loop 0
+putWord64N16 = loop 0
   where loop s n | s `seq` n `seq` False = undefined
         loop _ 0 = return ()
         loop s n = do
@@ -306,3 +311,274 @@ word64N16 = loop 0
           putWord64be (s+14)
           putWord64be (s+15)
           loop (s+16) (n-16)
+
+
+doGet :: Int -> Int -> Int -> Get Int
+doGet wordSize chunkSize =
+  case (wordSize, chunkSize) of
+    (1, 1)  -> fmap fromIntegral . getWord8N1
+    (1, 2)  -> fmap fromIntegral . getWord8N2
+    (1, 4)  -> fmap fromIntegral . getWord8N4
+    (1, 8)  -> fmap fromIntegral . getWord8N8
+    (1, 16) -> fmap fromIntegral . getWord8N16
+    (2, 1)  -> fmap fromIntegral . getWord16N1
+    (2, 2)  -> fmap fromIntegral . getWord16N2
+    (2, 4)  -> fmap fromIntegral . getWord16N4
+    (2, 8)  -> fmap fromIntegral . getWord16N8
+    (2, 16) -> fmap fromIntegral . getWord16N16
+    (4, 1)  -> fmap fromIntegral . getWord32N1
+    (4, 2)  -> fmap fromIntegral . getWord32N2
+    (4, 4)  -> fmap fromIntegral . getWord32N4
+    (4, 8)  -> fmap fromIntegral . getWord32N8
+    (4, 16) -> fmap fromIntegral . getWord32N16
+    (8, 1)  -> fmap fromIntegral . getWord64N1
+    (8, 2)  -> fmap fromIntegral . getWord64N2
+    (8, 4)  -> fmap fromIntegral . getWord64N4
+    (8, 8)  -> fmap fromIntegral . getWord64N8
+    (8, 16) -> fmap fromIntegral . getWord64N16
+
+getWord8N1 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord8
+          loop (s+s0) (n-1)
+
+getWord8N2 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord8
+          s1 <- getWord8
+          loop (s+s0+s1) (n-2)
+
+getWord8N4 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord8
+          s1 <- getWord8
+          s2 <- getWord8
+          s3 <- getWord8
+          loop (s+s0+s1+s2+s3) (n-4)
+
+getWord8N8 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord8
+          s1 <- getWord8
+          s2 <- getWord8
+          s3 <- getWord8
+          s4 <- getWord8
+          s5 <- getWord8
+          s6 <- getWord8
+          s7 <- getWord8
+          loop (s+s0+s1+s2+s3+s4+s5+s6+s7) (n-8)
+
+getWord8N16 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord8
+          s1 <- getWord8
+          s2 <- getWord8
+          s3 <- getWord8
+          s4 <- getWord8
+          s5 <- getWord8
+          s6 <- getWord8
+          s7 <- getWord8
+          s8 <- getWord8
+          s9 <- getWord8
+          s10 <- getWord8
+          s11 <- getWord8
+          s12 <- getWord8
+          s13 <- getWord8
+          s14 <- getWord8
+          s15 <- getWord8
+          loop (s+s0+s1+s2+s3+s4+s5+s6+s7+s9+s10+s11+s12+s13+s14+s15) (n-16)
+
+
+getWord16N1 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord16be
+          loop (s+s0) (n-1)
+
+getWord16N2 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord16be
+          s1 <- getWord16be
+          loop (s+s0+s1) (n-2)
+
+getWord16N4 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord16be
+          s1 <- getWord16be
+          s2 <- getWord16be
+          s3 <- getWord16be
+          loop (s+s0+s1+s2+s3) (n-4)
+
+getWord16N8 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord16be
+          s1 <- getWord16be
+          s2 <- getWord16be
+          s3 <- getWord16be
+          s4 <- getWord16be
+          s5 <- getWord16be
+          s6 <- getWord16be
+          s7 <- getWord16be
+          loop (s+s0+s1+s2+s3+s4+s5+s6+s7) (n-8)
+
+getWord16N16 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord16be
+          s1 <- getWord16be
+          s2 <- getWord16be
+          s3 <- getWord16be
+          s4 <- getWord16be
+          s5 <- getWord16be
+          s6 <- getWord16be
+          s7 <- getWord16be
+          s8 <- getWord16be
+          s9 <- getWord16be
+          s10 <- getWord16be
+          s11 <- getWord16be
+          s12 <- getWord16be
+          s13 <- getWord16be
+          s14 <- getWord16be
+          s15 <- getWord16be
+          loop (s+s0+s1+s2+s3+s4+s5+s6+s7+s9+s10+s11+s12+s13+s14+s15) (n-16)
+
+
+getWord32N1 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord32be
+          loop (s+s0) (n-1)
+
+getWord32N2 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord32be
+          s1 <- getWord32be
+          loop (s+s0+s1) (n-2)
+
+getWord32N4 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord32be
+          s1 <- getWord32be
+          s2 <- getWord32be
+          s3 <- getWord32be
+          loop (s+s0+s1+s2+s3) (n-4)
+
+getWord32N8 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord32be
+          s1 <- getWord32be
+          s2 <- getWord32be
+          s3 <- getWord32be
+          s4 <- getWord32be
+          s5 <- getWord32be
+          s6 <- getWord32be
+          s7 <- getWord32be
+          loop (s+s0+s1+s2+s3+s4+s5+s6+s7) (n-8)
+
+getWord32N16 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord32be
+          s1 <- getWord32be
+          s2 <- getWord32be
+          s3 <- getWord32be
+          s4 <- getWord32be
+          s5 <- getWord32be
+          s6 <- getWord32be
+          s7 <- getWord32be
+          s8 <- getWord32be
+          s9 <- getWord32be
+          s10 <- getWord32be
+          s11 <- getWord32be
+          s12 <- getWord32be
+          s13 <- getWord32be
+          s14 <- getWord32be
+          s15 <- getWord32be
+          loop (s+s0+s1+s2+s3+s4+s5+s6+s7+s9+s10+s11+s12+s13+s14+s15) (n-16)
+
+getWord64N1 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord64be
+          loop (s+s0) (n-1)
+
+getWord64N2 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord64be
+          s1 <- getWord64be
+          loop (s+s0+s1) (n-2)
+
+getWord64N4 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord64be
+          s1 <- getWord64be
+          s2 <- getWord64be
+          s3 <- getWord64be
+          loop (s+s0+s1+s2+s3) (n-4)
+
+getWord64N8 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord64be
+          s1 <- getWord64be
+          s2 <- getWord64be
+          s3 <- getWord64be
+          s4 <- getWord64be
+          s5 <- getWord64be
+          s6 <- getWord64be
+          s7 <- getWord64be
+          loop (s+s0+s1+s2+s3+s4+s5+s6+s7) (n-8)
+
+getWord64N16 = loop 0
+  where loop s n | s `seq` n `seq` False = undefined
+        loop s 0 = return s
+        loop s n = do
+          s0 <- getWord64be
+          s1 <- getWord64be
+          s2 <- getWord64be
+          s3 <- getWord64be
+          s4 <- getWord64be
+          s5 <- getWord64be
+          s6 <- getWord64be
+          s7 <- getWord64be
+          s8 <- getWord64be
+          s9 <- getWord64be
+          s10 <- getWord64be
+          s11 <- getWord64be
+          s12 <- getWord64be
+          s13 <- getWord64be
+          s14 <- getWord64be
+          s15 <- getWord64be
+          loop (s+s0+s1+s2+s3+s4+s5+s6+s7+s9+s10+s11+s12+s13+s14+s15) (n-16)
