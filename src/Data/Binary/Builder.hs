1fd397b2811ed76bf9c69e48db04f7af9b8c070d
Johan Tibell <johan.tibell@gmail.com>
2011-02-25 15:04:12 -0800

    Inline mconcat
    This results in better core and corresponding speed-ups.

--- src/Data/Binary/Builder.hs
+++ src/Data/Binary/Builder.hs
@@ -102,6 +102,8 @@ instance Monoid Builder where
     {-# INLINE mempty #-}
     mappend = append
     {-# INLINE mappend #-}
+    mconcat = foldr mappend mempty
+    {-# INLINE mconcat #-}
 
 ------------------------------------------------------------------------
 
