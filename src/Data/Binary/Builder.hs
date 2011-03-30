107c835e995c6512253bd7f678044cc9ba73f4a7
Johan Tibell <johan.tibell@gmail.com>
2011-03-30 13:04:45 +0200

    Replace writeNbytes by writeN and a cast
    This makes the rewrite rules fire for host order writes as well.

--- src/Data/Binary/Builder.hs
+++ src/Data/Binary/Builder.hs
@@ -234,21 +234,6 @@ newBuffer size = do
 {-# INLINE newBuffer #-}
 
 ------------------------------------------------------------------------
--- Aligned, host order writes of storable values
-
--- | Ensure that @n@ many bytes are available, and then use @f@ to write some
--- storable values into the memory.
-writeNbytes :: Storable a => Int -> (Ptr a -> IO ()) -> Builder
-writeNbytes n f = ensureFree n `append` withBuffer (writeNBufferBytes n f)
-{-# INLINE writeNbytes #-}
-
-writeNBufferBytes :: Storable a => Int -> (Ptr a -> IO ()) -> Buffer -> IO Buffer
-writeNBufferBytes n f (Buffer fp o u l) = do
-    withForeignPtr fp (\p -> f (p `plusPtr` (o+u)))
-    return (Buffer fp o (u+n) (l-n))
-{-# INLINE writeNBufferBytes #-}
-
-------------------------------------------------------------------------
 
 --
 -- We rely on the fromIntegral to do the right masking for us.
@@ -371,26 +356,26 @@ putWord64le w = writeN 8 $ \p -> do
 -- different endian or word sized machines, without conversion.
 --
 putWordhost :: Word -> Builder
-putWordhost w = writeNbytes (sizeOf (undefined :: Word)) (\p -> poke p w)
+putWordhost w = writeN (sizeOf (undefined :: Word)) (\p -> poke (castPtr p) w)
 {-# INLINE putWordhost #-}
 
 -- | Write a Word16 in native host order and host endianness.
 -- 2 bytes will be written, unaligned.
 putWord16host :: Word16 -> Builder
-putWord16host w16 = writeNbytes (sizeOf (undefined :: Word16)) (\p -> poke p w16)
+putWord16host w16 = writeN (sizeOf (undefined :: Word16)) (\p -> poke (castPtr p) w16)
 {-# INLINE putWord16host #-}
 
 -- | Write a Word32 in native host order and host endianness.
 -- 4 bytes will be written, unaligned.
 putWord32host :: Word32 -> Builder
-putWord32host w32 = writeNbytes (sizeOf (undefined :: Word32)) (\p -> poke p w32)
+putWord32host w32 = writeN (sizeOf (undefined :: Word32)) (\p -> poke (castPtr p) w32)
 {-# INLINE putWord32host #-}
 
 -- | Write a Word64 in native host order.
 -- On a 32 bit machine we write two host order Word32s, in big endian form.
 -- 8 bytes will be written, unaligned.
 putWord64host :: Word64 -> Builder
-putWord64host w = writeNbytes (sizeOf (undefined :: Word64)) (\p -> poke p w)
+putWord64host w = writeN (sizeOf (undefined :: Word64)) (\p -> poke (castPtr p) w)
 {-# INLINE putWord64host #-}
 
 ------------------------------------------------------------------------
