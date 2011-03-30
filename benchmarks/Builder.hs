7504aacf485e108d066202d72ab4013d924431e3
Johan Tibell <johan.tibell@gmail.com>
2011-03-30 16:45:09 +0200

    Add benchmark for strict ByteStrings

--- benchmarks/Builder.hs
+++ benchmarks/Builder.hs
@@ -1,6 +1,8 @@
 module Main (main) where
 
 import Criterion.Main (bench, defaultMain, whnf)
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Lazy as L
 import Data.Char (ord)
 import Data.Monoid (Monoid(mappend, mempty))
@@ -12,16 +14,29 @@ main :: IO ()
 main = defaultMain
     [ -- Test GHC loop optimization of continuation based code.
       bench "[Word8]" $ whnf (run . fromWord8s) word8s
-      
+
       -- Test bounds check merging
     , bench "bounds/[Word8]" $ whnf (run . from4Word8s) word8s
+
+    , bench "small ByteString" $ whnf (run . fromByteString) smallByteString
+    , bench "large ByteString" $ whnf (run . fromByteString) largeByteString
     ]
   where
     run = L.length . toLazyByteString
 
+-- Input data
+
 word8s :: [Word8]
 word8s = replicate 10000 $ fromIntegral $ ord 'a'
 {-# NOINLINE word8s #-}
+
+smallByteString :: S.ByteString
+smallByteString = C.pack "abcdefghi"
+
+largeByteString :: S.ByteString
+largeByteString = S.pack word8s
+
+-- Benchmarks
 
 fromWord8s :: [Word8] -> Builder
 fromWord8s [] = mempty
