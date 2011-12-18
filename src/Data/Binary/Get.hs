de96dec385aad1566d19db1eb7a5d452b9ee609c
Lennart Kolmodin <kolmodin@gmail.com>
2011-12-19 00:00:25 +0400

    Update my email.

--- src/Data/Binary/Get.hs
+++ src/Data/Binary/Get.hs
@@ -10,7 +10,7 @@
 -- Copyright   : Lennart Kolmodin
 -- License     : BSD3-style (see LICENSE)
 -- 
--- Maintainer  : Lennart Kolmodin <kolmodin@dtek.chalmers.se>
+-- Maintainer  : Lennart Kolmodin <kolmodin@gmail.com>
 -- Stability   : experimental
 -- Portability : portable to Hugs and GHC.
 --
