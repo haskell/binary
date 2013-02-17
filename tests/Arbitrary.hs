7e730355e22d9491dc1d90d3f62b9cb7e2351209
Lennart Kolmodin <kolmodin@gmail.com>
2013-02-17 23:38:13 +0400

    Cleanup instance Arbitrary of lazy bytestrings.

--- tests/Arbitrary.hs
+++ tests/Arbitrary.hs
@@ -8,7 +8,7 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 
 instance Arbitrary L.ByteString where
-    arbitrary     = arbitrary >>= return . L.fromChunks . filter (not. B.null) -- maintain the invariant.
+  arbitrary = fmap L.fromChunks arbitrary
 
 instance Arbitrary B.ByteString where
   arbitrary = B.pack `fmap` arbitrary
