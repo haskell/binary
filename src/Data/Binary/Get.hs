bf08660f1e1c292d7ae716d2351887371b8e3a9b
winterland1989 <winterland1989@gmail.com>
2016-09-21 22:45:42 +0800

    add runGetIncremental'

--- src/Data/Binary/Get.hs
+++ src/Data/Binary/Get.hs
@@ -145,6 +145,7 @@ module Data.Binary.Get (
     -- $incrementalinterface
     , Decoder(..)
     , runGetIncremental
+    , runGetIncremental'
 
     -- ** Providing input
     , pushChunk
@@ -228,7 +229,7 @@ import qualified Data.ByteString.Unsafe as B
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Internal as L
 
-import Data.Binary.Get.Internal hiding ( Decoder(..), runGetIncremental )
+import Data.Binary.Get.Internal hiding ( Decoder(..), runGetIncremental, runGetIncremental')
 import qualified Data.Binary.Get.Internal as I
 
 #if defined(__GLASGOW_HASKELL__) && !defined(__HADDOCK__)
@@ -283,6 +284,9 @@ data Decoder a = Fail !B.ByteString {-# UNPACK #-} !ByteOffset String
 -- 'pushEndOfInput'.
 runGetIncremental :: Get a -> Decoder a
 runGetIncremental = calculateOffset . I.runGetIncremental
+
+runGetIncremental' :: Get a -> B.ByteString -> Decoder a
+runGetIncremental' g = calculateOffset . I.runGetIncremental' g
 
 calculateOffset :: I.Decoder a -> Decoder a
 calculateOffset r0 = go r0 0
